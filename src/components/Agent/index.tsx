4e1e68f6a52442ca1c9d13aea86070e88b9d31d8
joaoferrariac <joaoferraridev@gmail.com>
2024-10-15 16:36:43 -0300

    :lipstick: style: add image to agent card

--- src/components/Agent/index.tsx
+++ src/components/Agent/index.tsx
@@ -11,17 +11,18 @@ interface AgentProps {
       displayIcon: string
       displayName: string
     }[]
+    fullPortrait: string
   }
 }
 
 export const Agent = ({ agent }: AgentProps) => (
   <a href="#" className={styles.agent}>
-    <div>
+    <div className={styles.text}>
       <p>{agent.role.displayName}</p>
       <strong>{agent.displayName}</strong>
     </div>
 
-    <ul>
+    <ul className={styles.abilities}>
       {agent.abilities.map(ability => (
         <li key={ability.displayName}>
           <Image
@@ -33,5 +34,9 @@ export const Agent = ({ agent }: AgentProps) => (
         </li>
       ))}
     </ul>
+
+    <div className={styles.background}>
+      <span style={{ backgroundImage: `url('${agent.fullPortrait}')` }} />
+    </div>
   </a>
 )
